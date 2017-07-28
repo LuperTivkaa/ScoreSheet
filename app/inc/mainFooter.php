ae2c68cd836d137f38cc922fec77eb6afc2373cc
Tivkaa Luper <tivkaa.luper@gmail.com>
2017-07-28 06:15:33 +0100

    Footer for publicly available site with injectable placeholders to be used by gulp to inject javascript

---
+++ app/inc/mainFooter.php
@@ -0,0 +1,93 @@
+<!-- second jumbotron begins here -->
+
+    <!-- jumbotron for key markup begins here -->
+<div class="jumbotron jumbotron-fluid newsletterJumbo leadp leadText mb-0">
+  <div class="container">
+
+  <!-- sign up row -->
+       <!-- signup form -->
+<hr>
+<div class="row py-2 text-muted">
+  <div class="col-md-6 col-xl-5">
+    <p><strong>About Facile</strong></p>
+    <p>Facile is a school management software that optimizes the day to day running of your school routine operations. It is developed and maintained by Lonax IT Technologies.</p>
+  </div>
+  <div class="col-md-6 col-xl-5 offset-xl-2">
+    <p><strong>Stay up-to-date with latest news</strong></p>
+    <div class="input-group">
+      <input type="text" class="form-control" placeholder="Email">
+      <span class="input-group-btn">
+        <button class="btn btn-primary" type="button">Sign up</button>
+      </span>
+    </div>
+  </div>
+</div>
+<hr>
+<!-- /signup form -->
+
+  </div>
+</div>
+
+<!-- second jumbotron begins here -->
+
+    <!-- jumbotron for key markup begins here -->
+<div class="jumbotron jumbotron-fluid bg-inverse leadp leadText mb-0">
+  <div class="container">
+    
+      <!-- create a new row -->
+      <div class="row">
+      <div class="col-md-4">
+      <h3 class="mb-2 leadText"><i class="fa fa-map-marker" aria-hidden="true"></i>
+Address</h3>
+      <p class="lead leadText leadp text-justify address">Lonax IT Solutions<br>
+      Eredivise Building, 2nd floor,<br>
+      Township Road,<br>
+      Abuja, Nigeria<br>
+      01-2909499<br>
+      Mail: info@lonax-it.com<br>
+      Web: www.lonax-it.com</p>
+      </div>
+
+      <div class="col-md-4">
+      <h3 class="mb-2 leadText"><i class="fa fa-link" aria-hidden="true"></i> Links</h3>
+          <ul class="fa-ul social">
+            
+            <li><i class="fa-li fa fa-home fa-fw" aria-hidden="true"></i><a href="index.php">Home</a></li>
+            <li><i class="fa-li fa fa-info fa-fw" aria-hidden="true"></i><a href="about.php">About Us</a></li>
+            <li><i class="fa-li fa fa-university fa-fw" aria-hidden="true"></i><a href="depts.php">Academics</a></li>
+            <!-- <li><i class="fa-li fa fa-user-plus fa-fw" aria-hidden="true"></i><a href="">Prospective Students</a></li> -->
+            <li><i class="fa-li fa fa-anchor fa-fw" aria-hidden="true"></i><a href="http://portal.nkstchtmkar.org" target="_blank">Portal</a></li>
+          </ul> 
+
+      </div>
+
+      <div class="col-md-4">
+      <h3 class="mb-2 leadText"><i class="fa fa-paper-plane" aria-hidden="true"></i> Connect With Us</h3>
+      <ul class="fa-ul social">
+            
+            <li><i class="fa-li fa fa-facebook-square fa-fw" aria-hidden="true"></i><a href="">Facebook</a></li>
+            <li><i class="fa-li fa fa-twitter-square fa-fw" aria-hidden="true"></i><a href="">Twitter</a></li>
+            <li><i class="fa-li fa fa-reddit-square fa-fw" aria-hidden="true"></i><a href="">Reddit</a></li>
+            <li><i class="fa-li fa fa-pinterest-square fa-fw" aria-hidden="true"></i><a href="">Pinterest</a></li>
+          </ul>
+      </div>
+
+      </div>
+
+  </div>
+</div>
+<!-- second jumbotron ends here -->
+</div><!-- /.modal -->
+    <!--  modal ends here-->
+    <!-- jQuery first, then Tether, then Bootstrap JS. -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js" integrity="sha384-3ceskX3iaEnIogmQchP8opvBy3Mi7Ce34nWjpBIwVTHfGYWQS9jwHDVRnpKKHJg7" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.3.7/js/tether.min.js" integrity="sha384-XTs3FgkjiBgo8qjEjBk0tGmf3wPrWtA6coPfQDfFEY8AnYJwjalXCiosYRBIBZX8" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.5/js/bootstrap.min.js" integrity="sha384-BLiI7JTZm+JWlgKa0M0kGRpJbF2J8q+qreVrKBC47e3K6BW78kGLrCkeRX6I9RoK" crossorigin="anonymous"></script>
+    <script type="text/javascript" src="javascript/app.js"></script>
+    <!-- inject:js -->
+    <!-- endinject -->
+  </body>
+</html>
+
+
+        
