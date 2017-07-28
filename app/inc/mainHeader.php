b09467817e021ee2879a7f50f3ab6f9f52ba72af
Tivkaa Luper <tivkaa.luper@gmail.com>
2017-07-28 06:16:24 +0100

    Haeder template with injectable placeholders to be used by gulp to insert css

---
+++ app/inc/mainHeader.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags always come first -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+
+<title> ScoreSheet | Home</title>
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.5/css/bootstrap.min.css" integrity="sha384-AysaV+vQoT3kOAXZkl02PThvDr8HYKPZhNT5h/CXfBThSRXQ6jW5DO2ekP5ViFdi" crossorigin="anonymous">
+    <link rel="stylesheet" href="fonts/css/font-awesome.min.css">
+    <link rel="stylesheet" type="text/css"
+          href="https://fonts.googleapis.com/css?family=Fjalla+One|Baloo+Chettan|Just+Another+Hand|Comfortaa|Dancing+Script">
+    <link rel="stylesheet" href="customCss/custom.css">
+    <!-- inject:css -->
+    <!-- endinject -->
+  </head>
+  <body id="home" data-spy="scroll" data-offset="100" data-target=".navbar">
+
+<nav class="navbar navbar-dark navbackground navbar-fixed-top">
+  <div class="navbar-header">
+     <!-- <a class="navbar-brand" href="#">Facile</a> -->
+  </div>
+  <button class="navbar-toggler hidden-sm-up pull-xs-right" type="button" data-toggle="collapse" data-target="#exCollapsingNavbar">
+    &#9776;
+  </button>
+  <div class="collapse navbar-toggleable-xs" id="exCollapsingNavbar">
+    <ul class="nav navbar-nav pull-xs-right">
+      <li class="nav-item">
+    <a class="" href="#home">Home <span class="sr-only">(current)</span></a>
+    </li>
+    <li class="nav-item">
+    <a class="" href="#features">Features</a>
+    </li>
+    <li class="nav-item">
+    <a class="" href="#pricing">Pricing</a>
+    </li>
+    <li class="nav-item">
+    <a class="" href="#modules">Modules</a>
+    </li>
+    <li class="nav-item">
+    <a class="" href="#testimonials">Testimonials</a>
+    </li>
+    <li class="nav-item">
+    <a class="" href="#about">About</a>
+    </li>
+    </ul>
+    <a class="navbar-brand float-sm-right mr-0 hidden-xs-down" href="#"> ScoreSheet</a>
+  </div>
+</nav>
+
+  <!-- navbar ends here -->
