59a5c9b5bc80acb4258b4c643ef5fd64b1e882bf
Tivkaa Luper <tivkaa.luper@gmail.com>
2017-07-31 21:41:56 +0100

    admin session

---
+++ app/inc/adminSession.php
@@ -0,0 +1,15 @@
+<?php
+ob_start(); 
+session_start();
+if ($_SESSION['user_info'][2] == "3" || $_SESSION['user_info'][2] == "4" || $_SESSION['user_info'] != "") 
+{ 
+
+}
+else
+{
+header("location:../index.php");
+exit(); 
+//echo ($_SESSION['user_info'][2]);
+}
+ob_end_flush();
+ ?>
