20c461d7c38765195530cd4a65b8465fc083d1ea
Tivkaa Luper <tivkaa.luper@gmail.com>
2017-07-31 21:41:36 +0100

    admin side menu

---
+++ app/inc/admin-sidemenu.php
@@ -0,0 +1,24 @@
+<div class="col-md-3" id="side-menu-bar">
+  
+  <div class="card">
+  <div class="card-header">
+    <i class="fa fa-bars" aria-hidden="true"></i>  Menu
+  </div>
+  <div class="card-block">
+      
+<ul class="fa-ul">
+	<li><a href="index.php"><i class="fa-li fa fa-home fa-fw"></i>Home</a></li>
+    <li><a href="myInstitution.php"><i class="fa-li fa fa-university fa-fw"></i>My Institution</a></li>
+    <li><a href="uploadLogo.php"><i class="fa-li fa fa-graduation-cap fa-fw"></i>Institution Logo</a></li>
+    <li><a href="myStaff.php"><i class="fa-li fa fa-user fa-fw"></i>My Staff</a></li>
+    <li><a href="myStudent.php"><i class="fa-li fa fa-camera fa-fw"></i>My Student/Pupil</a></li>
+	  <li><a href="sendTicket.php"><i class="fa-li fa fa-camera fa-fw"></i>Create Ticket</a></li>
+    <li><a class="all-tickets" href="availableTickets.php"><i class="fa-li fa fa-archive fa-fw"></i>All Tickets</a> <span class="badge badge-pill badge-danger">20</span></li>
+    <li><a href="settings.php"><i class="fa-li fa fa-sliders fa-fw"></i>Fee Settings</a></li>
+    <li><a href="academicSettings.php"><i class="fa-li fa fa-sliders fa-fw"></i>Academic Settings</a></li>
+    <li><a href="logout.php"><i class="fa-li fa fa-power-off fa-fw"></i>Log Out</a></li>
+</ul>
+  </div>
+</div>
+
+</div>
