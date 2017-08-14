4b79e7d51da907a47e6cf095c3912c0eb46e409f
Tivkaa Luper <tivkaa.luper@gmail.com>
2017-08-14 20:07:49 +0100

    Academic term script fixed

--- app/admin/academicTerm.php
+++ app/admin/academicTerm.php
@@ -1,23 +1,23 @@
 <?php 
-include 'inc/regSession.php';
-include 'inc/autoload.php';
+// include 'inc/regSession.php';
+// include 'inc/autoload.php';
 //$clientObj = new student();
-$clientid = $_SESSION['user_info'][4];
+//$clientid = $_SESSION['user_info'][4];
 //echo $clientObj->loadStaff($clientid);
 ?>
 
-              <!-- create fee items -->
+  <!-- create fee items -->
               <div id="fee-item-container">
 
-              <h4 class="right-menu-header ">Create Academic Term</h4>
+              <h5 class="top-header">Create Academic Term</h5>
               <div class="row">
 
-              <div class="form-group col-md-6">
+              <div class="form-group col-md-12">
                  <label for="term">Term Name</label>
                 <input type="text" class="form-control" id="term" name="term" placeholder="Enter Term"> 
               </div>
 
               </div>
-               <hr class="mb-2">
-              <button class="btn btn-primary btn-lg" id="add-term">Add Academic Term</button>
+              <button class="btn btn-primary btn-md" id="add-term">Add Academic Term</button>
             </div>
+              
