fbb05f595ee0c2dcd293a8ac7af61e4d4b54eb0c
Tivkaa Luper <tivkaa.luper@gmail.com>
2017-08-14 20:38:33 +0100

    drop dowm menu js sscripts

---
+++ app/js/new-design.js
@@ -0,0 +1,34 @@
+/* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
+//=============================
+//TO DO: USE THIS DESIGN FOR THE ACTUAL IMPLEMENTATION
+//=======
+$('.dropbtn').on('click', function(evt) {
+    //THIS IS Jquery's solution to the drop down menu
+    //TODO: FIX THE BEHAVIOUR OF THE MENU ON NOT CLOSING ON CLICKING ANOTHER ONE PREVIOUS TO IT
+    //CHECK BEFORE FIX
+    evt.preventDefault();
+    $('#myDropdown').removeClass('show');
+    $(this).next().addClass('show');
+
+});
+//========================
+//plain javascript dropd down script
+// function myFunction() {
+//     document.getElementById("myDropdown").classList.toggle("show");
+// }
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+        var i;
+        for (i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
+    }
+};
