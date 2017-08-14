15b9f34d993d3944fcddcfe815c353b068840d26
Tivkaa Luper <tivkaa.luper@gmail.com>
2017-08-14 20:09:16 +0100

    GUI fixed

--- app/admin/createSessions.php
+++ app/admin/createSessions.php
@@ -1,18 +1,11 @@
-<?php 
-include 'inc/regSession.php';
-include 'inc/autoload.php';
-//$clientObj = new student();
-//$clientid = $_SESSION['user_info'][4];
-//echo $clientObj->loadStaff($clientid);
-?>
 
               <!-- create fee items -->
               <div id="fee-item-container">
 
-              <h4 class="right-menu-header ">Create Academic Session(s)</h4>
+              <h5 class="top-header">Create Academic Session(s)</h5>
               <div class="row">
 
-              <div class="form-group col-md-6">
+              <div class="form-group col-md-12">
                  <label for="session">Session</label>
                 <input type="text" class="form-control" id="session" name="session" placeholder="session"> 
               </div>
