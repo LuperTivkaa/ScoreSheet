730d708312b862e47d70e318cbe387ec2f2ba56c
Tivkaa Luper <tivkaa.luper@gmail.com>
2017-08-14 20:11:08 +0100

    New index file in the admin replacing the old one

--- app/admin/index.php
+++ app/admin/index.php
@@ -1,78 +1,74 @@
-<?php 
-include 'inc/regSession.php';
-//load classes
-include 'inc/autoload.php';
-include 'inc/topHeader.php';
-$newClient = new client();
-$clientid = $_SESSION['user_info'][4];
-?>
-<title>ScoreSheet | Home </title>
-<?php include'inc/userProfileHeader.php'; ?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <title>ScoreSheet| Admin </title>
+    <?php include '../inc/scoresheet-header.php';?>
+
+    <body>
+        <div class="wrap">
+            <?php include '../inc/scoresheet-user-profile.php';?>
+
+            <?php include '../inc/scoresheet-school-profile.php';?>
+
+            <?php include '../inc/scoresheet-nav.php';?>
+
+            <div class="container">
+
+                <div class="primary-col">
+                    <p>This iisi iisn is isins kisghjghj jjkjkhjk jkjhkjh jbkjhkjh kjhkjhkjh kjbkjhkjhbkjhkjhkjh nmmn nn</p>
+                    <p>this </p>
+                    <!--outside content row  -->
+                    <div class="row">
+                        <!--column 8  -->
+                        <div class="col-md-8">
+                            <!--content row  -->
+                            <div class="row">
+                                <div class="form-group col-md-4">
+                                    <label for="student">Select Student</label>
+                                    <select class="custom-select form-control" id="student">
+                    </select>
+                                </div>
+                                <div class="form-group col-md-4">
+                                    <label for="student">Select Student</label>
+                                    <select class="custom-select form-control" id="student">
+                    </select>
+                                </div>
+                                <div class="form-group col-md-4">
+                                    <label for="student">Select Student</label>
+                                    <select class="custom-select form-control" id="student">
+                    </select>
+                                </div>
+                            </div>
+                            <!--end content row  -->
 
 
-<div  class="container mt-3">
-<?php
-include 'inc/institutionProfile.php';
-?>
+                        </div>
+                        <!--end cloumn 8  -->
 
-<div class="row">
+                        <div class="col-md-4">
+                            <div class="row">
+                                <div class="form-group col-md-6">
+                                    <label for="add-no">Admission Number</label>
+                                    <select class="custom-select form-control" id="add-no" disabled>
+                    </select>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <!--end outside row  -->
+                    <div id="my-info">
+                    </div>
 
-<!-- include side menu -->
-<?php include'inc/sidemenu.php';?>
-<!--end of  side menu include-->
-    
-<!--    div for main content-->
-<div class="col-md-6">
-  <!-- overview row -->
-<div class="row mb-2">
+                    <div id="new-content">
+                    </div>
 
-  <div class="col-md-3">
-            <h6 class="right-menu-header"><i class="fa fa-area-chart" aria-hidden="true"></i>
- Income</h6> <span class="badge badge-pill badge-danger">Today: 20,000</span>
-          
-        
-  </div>
+                </div>
 
-  <div class="col-md-3">
-    
-        <h6 class="right-menu-header"><i class="fa fa-graduation-cap" aria-hidden="true"></i> Students</h6>
-          <span class="badge badge-pill badge-danger">397</span>    
-  </div>
+                <?php include '../inc/scoresheet-secondary-col.php';?>
+            </div>
+            <?php include '../inc/scoresheet-footer.php';?>
+    </body>
 
-  <div class="col-md-3">
-        <h6 class="right-menu-header"><i class="fa fa-tasks" aria-hidden="true"></i> Task</h6>
-        <span class="badge badge-pill badge-danger">6</span>
-
-  </div>
-
-  <div class="col-md-3">
-        <h6 class="right-menu-header"><i class="fa fa-birthday-cake" aria-hidden="true"></i>
-Birthdays</h6>
-<span class="badge badge-pill badge-danger">Hurray 16</span>
-
-  </div>
-
-</div>
-
-<!-- end of overview -->
-<hr>
-<!-- div for message -->
-<div id="new-content">
-
-</div>
-
-</div>
-<!--    end of div for main content-->
-<!-- div to display message -->
-    
-<!-- right menu side bar -->
-<?php 
-include 'inc/rightMenu.php';
-?>
-<!-- end of right menu side bar -->
-
-</div>
-</div>
-
-   <!-- include footer -->
-<?php include 'inc/footer.php'; ?>
+</html>
