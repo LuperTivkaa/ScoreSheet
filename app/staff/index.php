d606790ced1452728e16f03b8a4cb17099486b96
Tivkaa Luper <tivkaa.luper@gmail.com>
2017-08-23 13:24:08 +0100

    New staff index file

--- app/staff/index.php
+++ app/staff/index.php
@@ -1,77 +1,43 @@
-<?php 
-include 'inc/regSession.php';
-//load classes
-include 'inc/autoload.php';
-include 'inc/topHeader.php';
-$newClient = new client();
-//$clientid = $_SESSION['user_info'][0];
-?>
-<title>ScoreSheet | Staff...</title>
-<?php include'inc/userProfileHeader.php'; ?>
+<!DOCTYPE html>
+<html lang="en">
 
+<head>
+    <meta charset="utf-8">
+    <title>ScoreSheet| Staff </title>
+    <?php include '../inc/scoresheet-header.php';?>
 
-<div  class="container mt-3">
-<?php
-include 'inc/institutionProfile.php';
-?>
+    <body>
+        <div class="wrap">
+            <?php include '../inc/scoresheet-user-profile.php';?>
 
-<div class="row">
+            <?php include '../inc/scoresheet-school-profile.php';?>
 
-<!-- include side menu -->
-<?php include'inc/sidemenu.php';?>
-<!--end of  side menu include-->
-    
-<!--    div for main content-->
-<div class="col-md-6">
-  <!-- overview row -->
-<div class="row mb-2">
+            <?php include '../inc/scoresheet-nav-staff.php';?>
 
-  <div class="col-md-3">
-            <h6 class="right-menu-header"><i class="fa fa-area-chart" aria-hidden="true"></i>
- My Class</h6> <span class="badge badge-pill badge-danger">Today: 20,000</span>
-     
-  </div>
+            <div class="wrapper">
 
-  <div class="col-md-3">
-    
-        <h6 class="right-menu-header"><i class="fa fa-graduation-cap" aria-hidden="true"></i> Students</h6>
-          <span class="badge badge-pill badge-danger">397</span>    
-  </div>
+                <div class="primary-col">
+                       <!--bootstrap Container   -->
+                   <div class="container">
+            
+                       <div class="row">
+                       
 
-  <div class="col-md-3">
-        <h6 class="right-menu-header"><i class="fa fa-tasks" aria-hidden="true"></i> Task</h6>
-        <span class="badge badge-pill badge-danger">6</span>
+                      <p>Content here th hjhis  htyn ysjjs tjjs tjjs tjjsthj sttsjjs tj tjis thshs tjjs tjj tjsg tj shhsbbs s hs  hjjshfs kjkjfskjfhds kjnfkjdskjds kjnsjnfs kjnjfndsf kjnsdjkfndsf kjndkjnskjf knkfnsdfks kjnsdfkndskjf knfkjdnfkd kjnfkjdnfkd kjndfkjndsfkjs kjnskjfnsdjf kjnskjfndskjf kjnfkjdsnfkj kjdnfkjdnfkdsjf knfkjdsnfkjd </p>
+                        </div>
+                      </div>
+                  <!--end bootstrap container  -->
+                    <div id="my-info">
+                    </div>
 
-  </div>
+                    <div id="new-content">
+                    </div>
 
-  <div class="col-md-3">
-        <h6 class="right-menu-header"><i class="fa fa-birthday-cake" aria-hidden="true"></i>
-Birthdays</h6>
-<span class="badge badge-pill badge-danger">Hurray 16</span>
+                </div>
 
-  </div>
+                <?php include '../inc/scoresheet-secondary-col.php';?>
+            </div>
+            <?php include '../inc/scoresheet-footer.php';?>
+    </body>
 
-</div>
-
-<!-- end of overview -->
-<hr>
-<!-- div for message -->
-<div id="new-content">
-
-</div>
-
-</div>
-<!--    end of div for main content-->
-<!-- div to display message -->
-    
-<!-- right menu side bar -->
-<?php 
-include 'inc/rightMenu.php';
-?>
-<!-- end of right menu side bar -->
-
-</div>
-</div>
-
-   <!-- include footer -->
-<?php include 'inc/footer.php'; ?>
+</html>
