c9aa7b04ca970e0e96c67530eba1ca26fbe76866
Tivkaa Luper <tivkaa.luper@gmail.com>
2017-08-23 13:19:03 +0100

    scoresheet nav for staff

---
+++ app/inc/scoresheet-nav-staff.php
@@ -0,0 +1,45 @@
+    <div class="menu-nav">
+            <div class="menu-nav-content">
+                <ul class="menu-nav-list">
+                    <li>
+                        <div class="dropdown">
+                    <a href="" class="dropbtn"><i class="fa fa-user fa-fw" aria-hidden="true"></i>Staff Profile <i class="fa fa-caret-down dropbtn" aria-hidden="true"></i>
+                    </a>
+                            <div id="myDropdown" class="dropdown-content">
+                                <a href="my-school.php"><i class="fa fa-user-plus fa-fw" aria-hidden="true"></i> Profile DashBoard</a>
+            
+                            </div>
+                        </div>
+                    </li>
+                     <li>
+                        <div class="dropdown">
+                            <a href="" class="dropbtn"><i class="fa fa-book fa-fw" aria-hidden="true"></i>Academics <i class="fa fa-caret-down dropbtn" aria-hidden="true"></i>
+</a>
+                            <div id="myDropdown" class="dropdown-content">
+                                <a href=""><i class="fa fa-th-large fa-fw" aria-hidden="true"></i>Academic Routines</a>
+            
+                            </div>
+                        </div>
+                    </li>
+                    <li>
+                    <li>
+                        <div class="dropdown">
+                            <a href="" class="dropbtn"><i class="fa fa-files-o fa-fw" aria-hidden="true"></i>Result <i class="fa fa-caret-down dropbtn" aria-hidden="true"></i></a>
+                            <div id="myDropdown" class="dropdown-content">
+                                <a href="my-staff.php"><i class="fa fa-th fa-fw" aria-hidden="true"></i>Result Processing</a>
+                            </div>
+                        </div>
+                    </li>
+                    <li>
+                        <div class="dropdown">
+                            <a href="#" class="dropbtn"><i class="fa fa-wrench fa-fw" aria-hidden="true"></i> Settings <i class="fa fa-caret-down dropbtn" aria-hidden="true"></i></a>
+                            <div id="myDropdown" class="dropdown-content">
+                                <a href="academic-settings.php"><i class="fa fa-paperclip fa-fw" aria-hidden="true"></i>Academic Settings</a>
+                                <a href="academic-manager.php"><i class="fa fa-paperclip fa-fw" aria-hidden="true"></i>Manage Academic Settings</a>
+                                 <a href="feeSettings.php"><i class="fa fa-paperclip fa-fw" aria-hidden="true"></i>Fee Settings</a>  
+                            </div>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+        </div>
