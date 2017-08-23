99479c141b8a473161c03d3253a2a88b5d5efb4f
Tivkaa Luper <tivkaa.luper@gmail.com>
2017-08-23 13:01:23 +0100

    Added academic manager link

--- app/inc/scoresheet-nav.php
+++ app/inc/scoresheet-nav.php
@@ -35,6 +35,7 @@
                             <a href="#" class="dropbtn"><i class="fa fa-wrench fa-fw" aria-hidden="true"></i> Settings <i class="fa fa-caret-down dropbtn" aria-hidden="true"></i></a>
                             <div id="myDropdown" class="dropdown-content">
                                 <a href="academic-settings.php"><i class="fa fa-paperclip fa-fw" aria-hidden="true"></i>Academic Settings</a>
+                                <a href="academic-manager.php"><i class="fa fa-paperclip fa-fw" aria-hidden="true"></i>Manage Academic Settings</a>
                                  <a href="feeSettings.php"><i class="fa fa-paperclip fa-fw" aria-hidden="true"></i>Fee Settings</a>  
                             </div>
                         </div>
