7169c087f2c1c9073a657705d7c41e00eb5e7419
Tivkaa Luper <tivkaa.luper@gmail.com>
2017-09-12 09:56:07 +0100

    Refactored the script to work with namespaces

--- app/staff/addTerminalExam.php
+++ app/staff/addTerminalExam.php
@@ -1,15 +1,18 @@
 <?php
-//session_start();
-include 'inc/regSession.php';
-//get the user id of the staff logged in through the session variable
-$staff_userid = $_SESSION['user_info'][0];
-//school id based on the logged in user_info
+session_start();
+require '../../vendor/autoload.php';
+use ScoreSheet\dbConnection;
+use ScoreSheet\client;
+use ScoreSheet\student;
+use ScoreSheet\staff;
+//use \PDO;
+$dbConnection = new dbConnection();
+$student = new student($dbConnection);
+$client = new client($dbConnection);
+$staff = new staff($dbConnection);
 $schid = $_SESSION['user_info'][4];
 
-//Autoload classes
-include'inc/autoload.php';
-$client_module = new client();
-$staff_module = new staff();
+$staffid = $_SESSION['user_info'][0];
 $dateCreated = date("Y-m-d");
 
 if ($_SERVER["REQUEST_METHOD"]=="POST")
@@ -18,15 +21,17 @@ if ($_SERVER["REQUEST_METHOD"]=="POST")
       $stud_regnumber = filter_input(INPUT_POST, "regno", FILTER_SANITIZE_STRING);
       $exam_scores = filter_input(INPUT_POST, "scores", FILTER_SANITIZE_NUMBER_INT);
       $studentClass = filter_input(INPUT_POST, "studentClass", FILTER_SANITIZE_NUMBER_INT);
-      $studentClass_arm= filter_input(INPUT_POST, "class_Arm", FILTER_SANITIZE_NUMBER_INT);
+      //$ca_no= filter_input(INPUT_POST, "ca_number", FILTER_SANITIZE_NUMBER_INT);
       $subject = filter_input(INPUT_POST, "subject", FILTER_SANITIZE_NUMBER_INT);
       //check for empty variable
-        if(empty($stud_regnumber) || empty($exam_scores) || empty($studentClass) || empty($studentClass_arm) || empty($subject))
+        if(empty($stud_regnumber) || empty($exam_scores) || empty($studentClass)  || empty($subject) )
         {
-            exit("Please fill all the fields...");
-        }else{
-      //CALL METHOD TO ADD CA
-      $staff_module->addTerminalExam($exam_scores,$stud_regnumber,$subject,$studentClass_arm,$staffid,$schid,$dateCreated);
+        exit("Please fill all the fields...");
+        }
+        else
+        {
+        //CALL METHOD TO ADD CA
+        $staff->addTerminalExam($exam_scores,$subject,$studentClass,$staffid,$schid,$stud_regnumber,$dateCreated);
         }
    }
 else
