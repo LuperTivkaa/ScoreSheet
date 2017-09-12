9271323575e5a99253a3fa87a62a100e51936ea8
Tivkaa Luper <tivkaa.luper@gmail.com>
2017-09-12 10:18:38 +0100

    Refactored code to use namespaces

--- app/staff/listSubjects.php
+++ app/staff/listSubjects.php
@@ -1,16 +1,23 @@
 <?php
-
-//including classes
-include 'inc/autoload.php';
-
-$myClient = new client();
+session_start();
+require '../../vendor/autoload.php';
+use ScoreSheet\dbConnection;
+use ScoreSheet\client;
+use ScoreSheet\student;
+//use \PDO;
+$dbConnection = new dbConnection();
+$student = new student($dbConnection);
+$client = new client($dbConnection);
+$clientid = $_SESSION['user_info'][4];
+//$newStaff = new student();
+$userid = $_SESSION['user_info'][0];
 
 if ($_SERVER["REQUEST_METHOD"]=="POST")
 {
 $id = filter_input(INPUT_POST, "id", FILTER_SANITIZE_NUMBER_INT);
 // $app->setPin($id);
 // $did = $app->getPin();
-$myClient->loadSubject($id);
+$client->loadSubject($id);
 }
 else
 {
