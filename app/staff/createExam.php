8767438be4489424f676426d7d5ecf439391ebb1
Tivkaa Luper <tivkaa.luper@gmail.com>
2017-09-12 10:12:49 +0100

    Removed class arm input  and refactored

--- app/staff/createExam.php
+++ app/staff/createExam.php
@@ -4,10 +4,13 @@ require '../../vendor/autoload.php';
 use ScoreSheet\dbConnection;
 use ScoreSheet\client;
 use ScoreSheet\student;
+use ScoreSheet\staff;
 //use \PDO;
 $dbConnection = new dbConnection();
 $student = new student($dbConnection);
 $client = new client($dbConnection);
+$staff = new staff($dbConnection);
+
 $clientid = $_SESSION['user_info'][4];
 //$newStaff = new student();
 $clientid = $_SESSION['user_info'][4];
@@ -16,44 +19,29 @@ $userid = $_SESSION['user_info'][0];
 
 <!--Row  to hold some sub menu  -->
 <div class="row">
-                       <div class="col-md-5 mt-1">
+                       
+
+                    <div class="col-md-12 mt-1">
                         <div class="row">
-                            <div class="col-6">
-                            <label for="class">Select Class</label>
-                            <select class="custom-select  form-control" id="class">
+                            
+
+                        <div class="col-6">
+                            <label for="studclass">Select Class</label>
+                            <select class="custom-select  form-control" id="studclass">
                               <?php
                                 $student->loadClass($clientid);
                               ?>
                             </select>
                             </div>
+                            
                             <div class="col-6">
-                            <label for="class-desc">Class Arm</label>
-                            <select class="custom-select  form-control" id="class-desc">
+                            <label for="listsubject">Subject</label>
+                            <select class="custom-select  form-control" id="listsubject">
+                            <?php
+                                $student->staffSubject($userid);
+                            ?>
                             </select>
-                            </div>
-                        </div>
-                       </div>
-
-                    <div class="col-md-7 mt-1">
-                        <div class="row">
-                            <div class="col-4">
-                            <label for="list-subject">Subject</label>
-                            <select class="custom-select  form-control" id="list-subject">
-                              <?php
-                              $student->loadSubject($userid);
-                              ?>
-                            </select>
-                            </div>
-                            <div class="col-4">
-                            <label for="arm">Term</label>
-                            <select class="custom-select  form-control" id="arm">
-                            </select>
-                            </div>
-                            <div class="col-4">
-                            <label for="arm">Session</label>
-                            <select class="custom-select  form-control" id="arm">
-                            </select>
-                            </div>
+                            </div>              
                         </div>  
                     </div>
   </div>
@@ -67,20 +55,20 @@ $userid = $_SESSION['user_info'][0];
       </div>
 
       <div class="col-md-8">
-        <h6 class="top-header text-xs-center mt-3">Add Exam Scores</h6>
+        <h6 class="top-header text-xs-center mt-3">Add Examination Scores</h6>
         <div class="row">
           
           <div class="col-md-6">
-                <label for="regno">Scores</label>
-                <input type="text" class="form-control" id="scores" name="regno" placeholder="CA Scores">
+                <label for="scores">Scores</label>
+                <input type="text" class="form-control" id="scores" name="scores" placeholder="Exam Scores">
             </div>
 
             <div class="col-md-6">
                 <label for="regno">Reg Number</label>
                 <input type="text" class="form-control" id="regno" name="regno" placeholder="Reg Number"> 
             </div>
-
-              <button class="submit btn btn-primary btn-md mt-3" id="add-ca">Enter Scores</button>
+              <span id="user_details">Display name of student here</span>
+              <button class="submit btn btn-primary btn-md mt-3" id="add-exam-scores">Enter Scores</button>
 
         </div>
       </div>
