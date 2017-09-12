d504078a4a1115fbb6c55e2f5d096ec0c4f31cfc
Tivkaa Luper <tivkaa.luper@gmail.com>
2017-09-12 09:51:58 +0100

    Refactored the script to work with namespaces

--- app/staff/addCa.php
+++ app/staff/addCa.php
@@ -1,34 +1,37 @@
 <?php
-//session_start();
-include 'inc/regSession.php';
-//get the user id of the staff logged in through the session variable
-$staffid = $_SESSION['user_info'][0];
-//school id based on the logged in user_info
+session_start();
+require '../../vendor/autoload.php';
+use ScoreSheet\dbConnection;
+use ScoreSheet\client;
+use ScoreSheet\student;
+use ScoreSheet\staff;
+//use \PDO;
+$dbConnection = new dbConnection();
+$student = new student($dbConnection);
+$client = new client($dbConnection);
+$staff = new staff($dbConnection);
 $schid = $_SESSION['user_info'][4];
+//$newStaff = new student();
+$staffid = $_SESSION['user_info'][0];
 
-//Autoload classes
-include'inc/autoload.php';
-$client_module = new client();
-$staff_module = new staff();
 $dateCreated = date("Y-m-d");
 
 if ($_SERVER["REQUEST_METHOD"]=="POST")
    {
       //$regno = $_SESSION['ID'];
-
       $stud_regnumber = filter_input(INPUT_POST, "regno", FILTER_SANITIZE_STRING);
       $ca_scores = filter_input(INPUT_POST, "scores", FILTER_SANITIZE_NUMBER_INT);
       $studentClass = filter_input(INPUT_POST, "studentClass", FILTER_SANITIZE_NUMBER_INT);
-      $studentClass_arm= filter_input(INPUT_POST, "class_Arm", FILTER_SANITIZE_NUMBER_INT);
+      $ca_number= filter_input(INPUT_POST, "ca_number", FILTER_SANITIZE_STRING);
       $subject = filter_input(INPUT_POST, "subject", FILTER_SANITIZE_NUMBER_INT);
 
       //check for empty variable
-        if(empty($stud_regnumber) || empty($ca_scores) || empty($studentClass) || empty($studentClass_arm) || empty($subject))
+        if(empty($stud_regnumber) || empty($ca_scores) || empty($studentClass) || empty($subject))
         {
             exit("Please fill all the fields...");
         }else{
             //CALL METHOD TO ADD CA
-            $staff_module->addCa($ca_scores,$stud_regnumber,$subject,$studentClass_arm,$staffid,$schid,$dateCreated);
+        $staff->addCa($ca_scores,$stud_regnumber,$subject,$studentClass,$staffid,$ca_number,$schid,$dateCreated);
         }
    }
 else
