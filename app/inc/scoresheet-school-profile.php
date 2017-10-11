9a2cc2a676a351eea84c6c2a62480a7a6ae8057a
Tivkaa Luper <tivkaa.luper@gmail.com>
2017-10-11 19:07:40 +0100

    Added school header code

--- app/inc/scoresheet-school-profile.php
+++ app/inc/scoresheet-school-profile.php
@@ -1,5 +1,18 @@
  <div class="secondary-nav">
             <div class="secondary-nav-content">
-                <h4>School Profile Name</h4>
+                <?php
+                //session_start();
+                require '../../vendor/autoload.php';
+                use ScoreSheet\dbConnection;
+                use ScoreSheet\client;
+                use ScoreSheet\student;
+                use ScoreSheet\staff;
+                //use \PDO;
+                $dbConnection = new dbConnection();
+                $student = new student($dbConnection);
+                $client = new client($dbConnection);
+                $clientid = $_SESSION['user_info'][4];
+                $client->schHeader($clientid);
+                ?>
             </div>
         </div>
