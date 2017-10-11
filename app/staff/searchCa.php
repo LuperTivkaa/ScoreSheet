c463e654b9ae0f95ee4a676b37bac262c7a26197
Tivkaa Luper <tivkaa.luper@gmail.com>
2017-10-11 20:01:43 +0100

    Edited elements name or id

--- app/staff/searchCa.php
+++ app/staff/searchCa.php
@@ -22,12 +22,12 @@ $userid = $_SESSION['user_info'][0];
 
                             <div class="col-12">
                                 <div class="input-group">
-                                <input type="text" class="form-control" id="tag" name="tag" placeholder="Search for CA...">
+                                <input type="text" class="form-control" id="stud-no" name="stud-no" placeholder="Search CA...">
                                 <span class="input-group-btn">
-                                    <button class="btn btn-primary" type="button">Search CA!</button>
+                                    <button class="btn btn-primary" type="button" id="ca-basic-search">Search CA!</button>
                                 </span>
                                 </div>
-                                <small id="emailHelp" class="form-text text-muted">You can search by ID or Surname.</small>
+                                <small id="hint" class="form-text text-muted">You can search by ID </small>
                             </div>
 
                         </div>
@@ -37,43 +37,40 @@ $userid = $_SESSION['user_info'][0];
                          <h5 class="top-header">Advanced Search</h5>
                         <div class="row">
                             
-                            <div class="col-4">
-                            <label for="tag">Select Subject</label>
-                            <select class="custom-select  form-control" id="ca-no">
-                            </select>
-                            
-                            </div>
-
-                            <div class="col-4">
-                            <label for="tag">Select Class</label>
-                            <select class="custom-select  form-control" id="arm">
-                            </select>
-                            </div>
-                            
-                            <div class="col-4">
-                                <label for="tag">Class Arm</label>
-                            <select class="custom-select  form-control" id="arm">
+                            <div class="col-6">
+                            <label for="my-class">Select Class</label>
+                            <select class="custom-select  form-control" id="my-class">
+                                <?php
+                                $student->loadClass($clientid);
+                            ?>
                             </select>
                             </div>
 
-                            <div class="col-4">
-                            <label for="tag">Session</label>
-                            <select class="custom-select  form-control" id="arm">
+                            <div class="col-6">
+                            <label for="position-subject">Select Subject</label>
+                            <select class="custom-select  form-control" id="position-subject">
                             </select>
                             </div>
 
-                            <div class="col-4">
-                            <label for="tag">Term</label>
-                            <select class="custom-select  form-control" id="arm">
+                            <div class="col-6">
+                            <label for="ca-session">Session</label>
+                            <select class="custom-select  form-control" id="ca-session">
+                                <?php
+                                $client->loadSession($clientid);
+                                ?>
                             </select>
                             </div>
 
-                            <div class="col-4">
-                            <label for="tag">Search Tag</label>
-                            <input type="text" class="form-control" id="tag" name="tag" placeholder="Search for CA...">
+                            <div class="col-6">
+                            <label for="ca-term">Term</label>
+                            <select class="custom-select  form-control" id="ca-term">
+                                <?php
+                            $client->loadTerm($clientid);
+                                ?>
+                            </select>
                             </div>
                             <div class="col-4">
-                            <button class="btn btn-primary mt-3" type="button">Search CA!</button>
+                            <button class="btn btn-primary mt-3" type="button" id="advanced-search-ca">Search CA!</button>
                             </div>
                         </div>  
                     </div>
