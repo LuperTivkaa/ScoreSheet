9dd70d6822d7409dab4f7086945d73c230f7f3fe
Tivkaa Luper <tivkaa.luper@gmail.com>
2017-10-11 20:10:58 +0100

    load scores after exam and ca edits

---
+++ app/staff/reloadCaScores.php
@@ -0,0 +1,40 @@
+<?php
+session_start();
+require '../../vendor/autoload.php';
+use ScoreSheet\dbConnection;
+use ScoreSheet\client;
+use ScoreSheet\student;
+use ScoreSheet\staff;
+use ScoreSheet\manager;
+//use \PDO;
+$dbConnection = new dbConnection();
+$student = new student($dbConnection);
+$client = new client($dbConnection);
+$staff = new staff($dbConnection);
+//$manager = new manager($dbConnection);
+$schid = $_SESSION['user_info'][4];
+//$newStaff = new student();
+$staffid = $_SESSION['user_info'][0];
+
+$dateCreated = date("Y-m-d");
+
+if ($_SERVER["REQUEST_METHOD"]=="POST")
+   {
+      //load subject scores after editing exam scores
+      $class = filter_input(INPUT_POST, "myclass", FILTER_SANITIZE_STRING);
+      $subject = filter_input(INPUT_POST, "subject", FILTER_SANITIZE_NUMBER_INT);
+      
+
+      //check for empty variable
+        if(empty($class) || empty($subject))
+        {
+            exit("Please fill all the fields...");
+        }else{
+            //reload exams after edit
+            $staff->reloadCa($class,$subject,$schid);
+        }
+   }
+else
+   {
+    echo "Please submit a record";
+   }
