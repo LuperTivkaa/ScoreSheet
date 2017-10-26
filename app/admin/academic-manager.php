88a095e0bec77315218ac6c57f05f0062f072062
Tivkaa Luper <tivkaa.luper@gmail.com>
2017-10-26 12:44:41 +0100

    Added modal div for various edit functionalities

--- app/admin/academic-manager.php
+++ app/admin/academic-manager.php
@@ -15,6 +15,119 @@
             <?php include '../inc/scoresheet-nav.php';?>
 
             <div class="wrapper">
+                <!-- The Modal -->
+                    <div id="myModal" class="modal-div">
+
+                    <!-- Modal content -->
+                    <div class="modal-content-div">
+                        <span class="closex">&times;</span>
+
+                    <div class="col-8">
+                        <h6 class="top-header text-xs-center mt-3"><i class="fa fa-plus-circle" aria-hidden="true"></i>Edit Menu  
+                        </h6>
+                        <p class="display">
+                           
+                        </p>
+                        <p id="modal_error"></p>  
+                         
+                       </div>
+
+                        <!--Edit term div -->
+                        <div class="term-div hide-me">
+
+                            <div class="col-8">
+                            <h6 class="highlight top-header text-xs-center mt-3">Edit Academic Term</h6>
+                            </div>
+
+                            <div class="col-8">
+                            <label for="editterm">Edit Term</label>
+                            <input type="text" class="form-control" id="editterm" name="editterm" placeholder="Enter Term"> 
+                            </div>
+                            
+                        <div class="col-4">
+                        <button class="submit btn btn-primary btn-md mt-3 mb-3" id="edit-term"><i class="fa fa-pencil" aria-hidden="true"></i> Update Term</button>
+                            </div>
+                        </div>
+                        <!--End edit term -->
+
+                        <!--Begin edit subject div  -->
+                        
+                        <div class="subject-div hide-me">
+                            <div class="col-8">
+                            <h6 class="highlight top-header text-xs-center mt-3"> Update Subject</h6>
+                            </div>
+
+                            <div class="col-8">
+                             <label for="editsubject">Subject Name:</label>
+                            <input type="text" class="form-control" id="editsubject" name="editsubject">
+                            </div>
+                            
+                        <div class="col-4">
+                        <button class="submit btn btn-primary btn-md mt-3 mb-3" id="edit-subject"><i class="fa fa-pencil" aria-hidden="true"></i> Update Subject</button>
+                        </div>
+                        </div>
+                        <!--end edit subject div  -->
+                        <!--begin edit class  -->
+                        <div class="class-div hide-me">
+                            <div class="col-8">
+                            <h6 class="highlight top-header text-xs-center mt-3">Edit Class</h6>
+                            </div>
+
+                            <div class="col-8">
+                             <label for="editclass">Class Name:</label>
+                            <input type="text" class="form-control" id="editclass" name="editclass">
+                            </div>
+                            
+                        <div class="col-4">
+                        <button class="submit btn btn-primary btn-md mt-3 mb-3" id="edit-class"><i class="fa fa-pencil" aria-hidden="true"></i> Update Class</button>
+                        </div>
+                        </div>
+                        <!--end edit class  -->
+
+                        <!--Begin edit prefix settings  -->
+                        <div class="prefix-div hide-me">
+                            <div class="col-8">
+                            <h6 class="highlight top-header text-xs-center mt-3">Edit Prefix Settings</h6>
+                            </div>
+
+                            <div class="col-8">
+                             <label for="editprefix">Prefix Settings:</label>
+                            <input type="text" class="form-control" id="editprefix" name="editprefix">
+                            </div>
+                            
+                        <div class="col-4">
+                        <button class="submit btn btn-primary btn-md mt-3 mb-3" id="edit-prefix"><i class="fa fa-pencil" aria-hidden="true"></i> Update Prefix</button>
+                        </div>
+                        </div>
+
+                        <!--End edit prefix settings  -->
+
+                        <!--Begin session div  -->
+                        <div class="session-div hide-me">
+                            <div class="col-8">
+                            <h6 class="highlight top-header text-xs-center mt-3"> Update Session</h6>
+                            </div>
+
+                            <div class="col-8">
+                            <label for="editsession">Edit Session</label>
+                            <input type="text" class="form-control" id="editsession" name="editsession"> 
+                            </div>
+
+                            <div class="col-4">
+                            <button class="submit btn btn-primary btn-md mt-3 mb-3" id="edit-session"><i class="fa fa-plus" aria-hidden="true"></i> Update Session</button>
+                            </div>
+
+
+                            </div>
+                            <input type="hidden" class="form-control" id="record-id" name="record-id">
+                            <input type="hidden" class="form-control" id="item-value" name="item-value">
+                            <hr>
+                            <div class="col-6" id="modal-list">
+                            </div>
+                            </div>
+
+                    </div>
+                <!--end new modal  -->
 
                 <div class="primary-col">
                         <div class="aux-menu">
@@ -23,8 +136,8 @@
                             <li><a class="load-url" href="myTerms.php"><i class="fa fa-paper-plane fa-fw" aria-hidden="true"></i>All Terms</a></li>
                             <li><a class="load-url" href="mySessions.php"><i class="fa fa-bars fa-fw" aria-hidden="true"></i> All Sessions</a></li>
                             <li><a class="load-url" href="mySubjects.php"><i class="fa fa-paperclip fa-fw" aria-hidden="true"></i>Manage Subjects</a></li>
-                            <li> <a class="load-url" href=""><i class="fa fa-plus fa-fw" aria-hidden="true"></i>Manage Classes</a></li>
-                            <li><a class="load-url" href=""><i class="fa fa-user-plus fa-fw" aria-hidden="true"></i> Manage Prefix Settings</a></li>
+                            <li> <a class="load-url" href="schoolClasses.php"><i class="fa fa-plus fa-fw" aria-hidden="true"></i>Manage Classes</a></li>
+                            <li><a class="load-url" href="myAdmNoSettings.php"><i class="fa fa-user-plus fa-fw" aria-hidden="true"></i> Manage Prefix Settings</a></li>
                             </ul>   
                         </div>  
                     <!--You can put content here inside the primary column  -->
