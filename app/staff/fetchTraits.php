ea2080b4a3cb397443a0ea6d27872fe8af81badc
Tivkaa Luper <tivkaa.luper@gmail.com>
2017-10-26 13:24:36 +0100

    added post variables

--- app/staff/fetchTraits.php
+++ app/staff/fetchTraits.php
@@ -4,9 +4,11 @@ require '../../vendor/autoload.php';
 use ScoreSheet\dbConnection;
 use ScoreSheet\client;
 use ScoreSheet\student;
+use ScoreSheet\staff;
 //use \PDO;
 $dbConnection = new dbConnection();
 $student = new student($dbConnection);
+$staff = new staff($dbConnection);
 $client = new client($dbConnection);
 $clientid = $_SESSION['user_info'][4];
 //$newStaff = new student();
@@ -14,7 +16,7 @@ $userid = $_SESSION['user_info'][0];
 
 if ($_SERVER["REQUEST_METHOD"]=="POST")
 {
-$class = filter_input(INPUT_POST, "myclass", FILTER_SANITIZE_NUMBER_INT);
+$classid = filter_input(INPUT_POST, "studentclass", FILTER_SANITIZE_NUMBER_INT);
 $session = filter_input(INPUT_POST, "session", FILTER_SANITIZE_NUMBER_INT);
 $term = filter_input(INPUT_POST, "term", FILTER_SANITIZE_NUMBER_INT);
 // $app->setPin($id);
