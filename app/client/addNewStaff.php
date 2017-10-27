cca27d7356a4c4673835efa0633bbc80dcad7e63
Tivkaa Luper <tivkaa.luper@gmail.com>
2017-10-27 14:04:53 +0100

    Added namespace

--- app/client/addNewStaff.php
+++ app/client/addNewStaff.php
@@ -1,24 +1,23 @@
 <?php
-//session_start();
-include 'inc/regSession.php';
-$clientid = $_SESSION['sess_info'][0];
-//function to generate unique number below
-//Autoload classes
-include'inc/autoload.php';
-$client = new client();
+session_start();
+require '../../vendor/autoload.php';
+use ScoreSheet\dbConnection;
+use ScoreSheet\client;
+//use \PDO;
+$dbConnection = new dbConnection();
+$client = new client($dbConnection);
+$clientid = $_SESSION['user_info'][4];
+
 $dateCreated = date("Y-m-d");
 
 if ($_SERVER["REQUEST_METHOD"]=="POST")
 {
 //$regno = $_SESSION['ID'];
-$email = filter_input(INPUT_POST, "email", FILTER_SANITIZE_STRING);
+$email = filter_input(INPUT_POST, "email", FILTER_SANITIZE_EMAIL);
 $username = filter_input(INPUT_POST, "username", FILTER_SANITIZE_STRING);
 $pass = filter_input(INPUT_POST, "pass", FILTER_SANITIZE_STRING);
 $role = filter_input(INPUT_POST, "role", FILTER_SANITIZE_STRING);
 
-
-$client->setEmail($email);
-$email = $client->getEmail();
 
 $client->setUserName($username);
 $user = $client->getUserName();
@@ -28,7 +27,8 @@ $pass = $client->getPassword();
 
 $client->setRole($role);
 $role = $client->getRole();
-
+$client->setEmail($email);
+$email = $client->getEmail();
 
 $client->newStaff($email,$username,$pass,$role,$clientid,$dateCreated,$dateCreated);
     
