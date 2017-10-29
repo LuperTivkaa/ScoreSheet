054d92167d005475178eed457d9eba4e41fe027e
Tivkaa Luper <tivkaa.luper@gmail.com>
2017-10-29 23:11:21 +0100

    Added code for adding class category

--- app/admin/createClass.php
+++ app/admin/createClass.php
@@ -1,25 +1,42 @@
-<?php 
-// include 'inc/regSession.php';
-// include 'inc/autoload.php';
-// $clientObj = new student();
-// $clientid = $_SESSION['user_info'][4];
-//echo $clientObj->loadStaff($clientid);
-?> 
-
+<?php
+session_start();
+require '../../vendor/autoload.php';
+use ScoreSheet\dbConnection;
+use ScoreSheet\client;
+use ScoreSheet\student;
+//use \PDO;
+$dbConnection = new dbConnection();
+$student = new student($dbConnection);
+$client = new client($dbConnection);
+$clientid = $_SESSION['user_info'][4];
+//$newStaff = new student();
+$userid = $_SESSION['user_info'][0];
+?>
               <!-- create fee items -->
               <div id="fee-item-container">
 
               <h5 class="top-header ">Create New Class</h5>
               <div class="row">
 
-            <div class="form-group col-md-12">
+            <div class="form-group col-md-6">
             <label for="class-name">Class Name:</label>
                 <input type="text" class="form-control" id="class-name" name="class-name" placeholder="E.g JSS1A or Primary 1 Gold">
             </div>
 
+            <div class="form-group col-md-6">
+            <label for="class-category">Class Category:</label>
+                <select class="custom-select form-control" id="class-category">
+                <?php
+                $client->classCategory($clientid);
+                ?>
+                </select>
+            </div>
+
               
               </div>
+              <div class="col-md-3">
               <button class="submit btn btn-primary" id="add-new-class">Add New Class</button>
+              </div>
 
               <hr>
               <!-- <div class="row">
