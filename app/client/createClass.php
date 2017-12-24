13241b01b3adf7b5b57d357b931427775bfb1576
Tivkaa Luper <tivkaa.luper@gmail.com>
2017-12-24 19:15:12 +0100

    Added classes

--- app/client/createClass.php
+++ app/client/createClass.php
@@ -1,47 +1,47 @@
-<?php 
-// include 'inc/regSession.php';
-// include 'inc/autoload.php';
-// $clientObj = new student();
-// $clientid = $_SESSION['user_info'][4];
-//echo $clientObj->loadStaff($clientid);
-?> 
+<?php
+session_start();
+require '../../vendor/autoload.php';
+use ScoreSheet\dbConnection;
+use ScoreSheet\client;
+use ScoreSheet\student;
+use ScoreSheet\staff;
+use Carbon\Carbon;
+//use \PDO;
+$dbConnection = new dbConnection();
+$client = new client($dbConnection);
+$student = new student($dbConnection);
+$staff = new staff($dbConnection);
 
+$clientid = $_SESSION['user_info'][4];
+$userid = $_SESSION['user_info'][0];
+$roleid = $_SESSION['user_info'][2];
+$staff->clientUser($roleid,$clientid);
+?>
               <!-- create fee items -->
               <div id="fee-item-container">
 
               <h5 class="top-header ">Create New Class</h5>
               <div class="row">
 
-            <div class="form-group col-md-12">
+            <div class="form-group col-md-6">
             <label for="class-name">Class Name:</label>
                 <input type="text" class="form-control" id="class-name" name="class-name" placeholder="E.g JSS1A or Primary 1 Gold">
             </div>
 
+            <div class="form-group col-md-6">
+            <label for="class-category">Class Category:</label>
+                <select class="custom-select form-control" id="class-category">
+                <?php
+                $client->classCategory($clientid);
+                ?>
+                </select>
+            </div>
+
               
               </div>
+              <div class="col-md-3">
               <button class="submit btn btn-primary" id="add-new-class">Add New Class</button>
+              </div>
 
               <hr>
-              <!-- <div class="row">
-              <div class="col-md-12 mb-3">
-              <h5 class="top-header text-xs-center mt-3">Specify Class Arm</h5><small>Select class and enter appropriate description. Click <i class="fa fa-refresh fa-fw reload-class" aria-hidden="true"></i> to load Classes.</small>
-              </div>
-              </div>
-
-              <div class="row">
-
-            <div class="form-group col-md-6">
-                <label for="class-list">Select Class</label>
-                <select class="custom-select form-control" id="class-list">
-                </select>
-              </div>
-
-              <div class="form-group col-md-6">
-                
-                 <label for="class-desc">Class Description</label>
-                <input type="text" class="form-control" id="class-desc" name="class-desc" placeholder="E.g Alpha or A">
-              </div>
-              
-              </div>
-              <button class="submit btn btn-primary" id="assign-desc">Assign Class Description</button>
-            </div> -->
+             
