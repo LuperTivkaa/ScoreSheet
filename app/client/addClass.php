70e74c29b02dc18774f997f1d497e3e687f361cf
Tivkaa Luper <tivkaa.luper@gmail.com>
2017-12-24 19:09:40 +0100

    minor refactoring and edits

--- app/client/addClass.php
+++ app/client/addClass.php
@@ -5,32 +5,32 @@ use ScoreSheet\dbConnection;
 use ScoreSheet\client;
 use ScoreSheet\student;
 use ScoreSheet\staff;
+use Carbon\Carbon;
 //use \PDO;
 $dbConnection = new dbConnection();
-$student = new student($dbConnection);
 $client = new client($dbConnection);
+$student = new student($dbConnection);
 $staff = new staff($dbConnection);
-//$jobmanager = new manager($dbConnection);
+
 $clientid = $_SESSION['user_info'][4];
-//$newStaff = new student();
 $userid = $_SESSION['user_info'][0];
-$myroleid = $_SESSION['user_info'][2];
-$staff->clientUser($myroleid,$clientid);
+$roleid = $_SESSION['user_info'][2];
+$staff->clientUser($roleid,$clientid);
 //$dateCreated = date("Y-m-d");
 
 if ($_SERVER["REQUEST_METHOD"]=="POST")
 {
 //$regno = $_SESSION['ID'];
 $class = filter_input(INPUT_POST, "myclass", FILTER_SANITIZE_STRING);
-//$pass = filter_input(INPUT_POST, "pass", FILTER_SANITIZE_STRING);
+  $category = filter_input(INPUT_POST, "category", FILTER_SANITIZE_NUMBER_INT);
 //$role = filter_input(INPUT_POST, "role", FILTER_SANITIZE_STRING);
 
-if(empty($class)){
-	exit("Please enter a class");
+if(empty($class) || empty($category)){
+	exit("Please enter a class/category");
 }
 
 
-$client->newClass($class,$clientid);
+$client->newClass($class,$category,$clientid);
     
 }
 else
