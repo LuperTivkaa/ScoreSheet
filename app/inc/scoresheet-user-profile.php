524939a6d1320902c0e7cad5aee2aef48ffaf9bf
Tivkaa Luper <tivkaa.luper@gmail.com>
2017-12-29 20:37:51 +0100

    Refactored to add staff profile picture and username

--- app/inc/scoresheet-user-profile.php
+++ app/inc/scoresheet-user-profile.php
@@ -1,23 +1,40 @@
+<?php
+//session_start();
+require '../../vendor/autoload.php';
+use ScoreSheet\dbConnection;
+use ScoreSheet\client;
+use ScoreSheet\student;
+use ScoreSheet\staff;
+//use \PDO;
+$dbConnection = new dbConnection();
+$student = new student($dbConnection);
+$client = new client($dbConnection);
+$staff = new staff($dbConnection);
+$schid = $_SESSION['user_info'][4];
+$staffid = $_SESSION['user_info'][0];
+?>
+
 <div class="primary-nav">
 
             <div class="primary-nav-content">
                 <h2 class="site-name">ScoreSheet</h2>
                 <ul class="primary-link">
                     <li>
-                        <?php echo $_SESSION['user_info'][1];?>
+                        <?php 
+                          $client->staffIconName($schid,$staffid);
+                        ?>
+                        <span><a href="./logout.php"><i class="fa fa-lock fa-fw" aria-hidden="true"></i> Log Out </a></span>
                     </li>
                     <li>
                     <?php
-                    if ($_SESSION['user_info'][2] == 3)
-                    {
-                        echo '<a href="../admin/index.php"><i class="fa fa-user-circle-o fa-fw" aria-hidden="true"></i> Admin</a>';
-                    }
-                        ?>
+                        if ($_SESSION['user_info'][2] == 3)
+                        {
+                            echo '<a href="../admin/index.php"><i class="fa fa-user-circle-o fa-fw" aria-hidden="true"></i> Admin</a>';
+                        }
+                    ?>
 
                     </li>
-                    <li>
-                        <a href="./logout.php"><i class="fa fa-lock fa-fw" aria-hidden="true"></i> Log Out </a
-                    </li>
+                   
                 </ul>
             </div>
 
