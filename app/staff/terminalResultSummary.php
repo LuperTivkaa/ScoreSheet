a3632175604234bd7f344430249794a1433b7b98
Tivkaa Luper <tivkaaluper@gmail.com>
2018-07-03 09:04:01 +0100

    terminal summary scripts

---
+++ app/staff/terminalResultSummary.php
@@ -0,0 +1,43 @@
+<?php
+session_start();
+require '../../vendor/autoload.php';
+use ScoreSheet\dbConnection;
+use ScoreSheet\client;
+use ScoreSheet\student;
+use ScoreSheet\staff;
+use ScoreSheet\printRoutines;
+//use \PDO;
+$dbConnection = new dbConnection();
+$student = new student($dbConnection);
+$staff = new staff($dbConnection);
+$printObj = new printRoutines($dbConnection);
+$client = new client($dbConnection);
+$clientid = $_SESSION['user_info'][4];
+$userid = $_SESSION['user_info'][0];
+
+$myroleid = $_SESSION['user_info'][2];
+$staff->staffUser($myroleid,$clientid);
+//$dateCreated = date("Y-m-d");
+
+if ($_SERVER["REQUEST_METHOD"]=="POST")
+   {
+      
+      $studclass = filter_input(INPUT_POST, "studclass", FILTER_SANITIZE_NUMBER_INT);
+      $session = filter_input(INPUT_POST, "session", FILTER_SANITIZE_NUMBER_INT);
+      $term = filter_input(INPUT_POST, "term", FILTER_SANITIZE_NUMBER_INT);
+      
+      //check for empty variable
+        if(empty($studclass) || empty($session)  || empty($term) )
+        {
+        exit("Please fill all the fields...");
+        }
+        else
+        {
+        //Display terminal result summary
+        $printObj->annualResultSummary($studclass,$term,$session,$clientid);
+        }
+   }
+else
+   {
+    echo "Please submit a record";
+   }
---
+++ app/staff/viewAnnualResult.php
@@ -0,0 +1,66 @@
+<?php
+session_start();
+require '../../vendor/autoload.php';
+use ScoreSheet\dbConnection;
+use ScoreSheet\client;
+use ScoreSheet\student;
+use ScoreSheet\staff;
+//use \PDO;
+$dbConnection = new dbConnection();
+$student = new student($dbConnection);
+$staff = new staff($dbConnection);
+$client = new client($dbConnection);
+$clientid = $_SESSION['user_info'][4];
+//$newStaff = new student();
+$userid = $_SESSION['user_info'][0];
+$myroleid = $_SESSION['user_info'][2];
+$staff->staffUser($myroleid,$clientid);
+?>
+
+<!--Row  to hold some sub menu  -->
+<div class="row">
+                    <div class="col-md-12 mt-1">
+                         <h5 class="top-header">View Terminal Result Summary</h6>
+                        <div class="row">
+
+                            <div class="col-12">
+                            <label for="studclass">Select Class</label>
+                            <select class="custom-select  form-control" id="studclass">
+                              <?php
+                                $client->loadClassTeacherClass($userid,$clientid);
+                              ?>
+                            </select>
+                    </div>
+
+                            <div class="col-6">
+                            <label for="session">Session</label>
+                            <select class="custom-select  form-control" id="session">
+                                <?php
+                                $staff->ActiveSession($clientid);
+                                ?>
+                            </select>
+                            </div>
+
+                            <div class="col-6">
+                            <label for="term">Term</label>
+                            <select class="custom-select  form-control" id="term">
+                                <?php
+                                $staff->ActiveTerm($clientid);
+                                ?>
+                            </select>
+                            </div>
+                            
+                        </div> 
+                        <div class="row"> 
+                        <div class="col-6">
+                    <button class="btn btn-primary mt-3" type="button" id="view-terminal-result-summary"><i class="fa fa-print" aria-hidden="true"></i> View Terminal Result</button>
+                            </div>
+                            </div>
+                    </div>
+  </div>
+<!-- Enter form to create new student here -->
+
+
+      
+
+           
